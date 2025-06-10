d39108ab6828b2fb8b6d05de70284209ae674c04
GockinD <gockindmitrij0@gmail.com>
2025-06-10 20:55:35 +0500

    Problem with homework4

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -58,8 +58,8 @@ public class StudentController {
     }
 
     @PostMapping
-    public ResponseEntity<Student> addStudent(@RequestBody Student student) {
-        return ResponseEntity.ok(studentService.addStudent(student));
+    public void addStudent(@RequestBody Student student) {
+        ResponseEntity.ok(studentService.addStudent(student));
     }
 
     @PutMapping
